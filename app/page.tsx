5f7b8cead2dcf870cb4f8fb01ed698ad82912e4e
Dominik Gadzina <epros98@gmail.com>
2024-12-31 12:13:21 +0100

    updated name of project and icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 const Home = () => {
   return (
     <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold text-center mb-6">Welcome to My Recipe App</h1>
-      
-   
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      
-      </div>
+      <h1 className="text-3xl font-bold text-center mb-6">
+        Welcome to My Recipe App
+      </h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6"></div>
     </div>
   );
 };
